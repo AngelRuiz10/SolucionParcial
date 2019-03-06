0fd499e3f4a16778049720befbfce0d130bba7af
Angel <abraruiza@gmail.com>
2019-03-05 20:39:12 -0600

    SolucionParcial

--- src/javaapplication11/JavaApplication11.java
+++ src/javaapplication11/JavaApplication11.java
@@ -1,46 +1,44 @@
 /*
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//
+Programa de la solucion del parcila de una tiendad el cual 
+trata que se ingrese un vector la cantidad de 5 nombres de producuto
+5 precio y 5 cantidades, al final realiza el calculo del precio por la cantidad.
+*/
 package javaapplication11;
 import java.util.Scanner;
 
 public class JavaApplication11 {
     Scanner teclado = new Scanner(System.in);
     String [] vectorNombre = new String[5];
-    int total=0;
-    int precio, cantidad;
-    int [] intPrecio = new int[5];
-    int [] intCantidad = new int [5];
-    public void datos(){
-       for (int nombre =0; nombre<vectorNombre.length; nombre++){
-        System.out.print("Ingrese el nombre del producto ");
-        vectorNombre[nombre] = teclado.nextLine();
+    int total=0; //variable total 
+    int precio, cantidad; //Varialbes para precio y cantidad
+    int [] intPrecio = new int[5];//vector precio
+    int [] intCantidad = new int [5]; //vector cantidad 
+    public void datos(){//Clase datos 
+       for (int nombre =0; nombre<vectorNombre.length; nombre++){ //Ciclo for que recorre el nombre
+        System.out.print("Ingrese el nombre del producto ");//Pide los datos del producto
+        vectorNombre[nombre] = teclado.nextLine();//Obtioene los datos mendiante el teclado
        }
     }
-       public void pre(){
-           for (precio = 0; precio<intPrecio.length; precio ++){
-               System.out.print("Ingrese el precio ["+(precio+1)+ "]   ");
-               intPrecio[precio] = teclado.nextInt();
+       public void pre(){//Clase para el precio 
+           for (precio = 0; precio<intPrecio.length; precio ++){//Ciclo for para el precio
+               System.out.print("Ingrese el precio ["+(precio+1)+ "]   ");//Pide los datos de precio
+               intPrecio[precio] = teclado.nextInt();//Obtioene los datos mendiante el teclado
            }
        }
-       public void cant(){
-           for (cantidad = 0; cantidad<intCantidad.length; cantidad++){
-               System.out.print("Ingrese la cantidad ["+(cantidad+1)+"]    ");
-               intCantidad[cantidad] = teclado.nextInt();
+       public void cant(){//Clase para cantidad
+           for (cantidad = 0; cantidad<intCantidad.length; cantidad++){//for para que recorre la cantidad
+               System.out.print("Ingrese la cantidad ["+(cantidad+1)+"]    ");//Pide los datos de cantidad
+               intCantidad[cantidad] = teclado.nextInt();//Obtioene los datos mendiante el teclado
            }
        }
-       public void mostrarNombre(){
-           for (int nombre =0; nombre<vectorNombre.length; nombre++){
-               System.out.print(vectorNombre[nombre]+ "  ");
+       public void mostrarNombre(){//Clase para mostrar el vector nombre
+           for (int nombre =0; nombre<vectorNombre.length; nombre++){//Ciclo for que para mostrar el nombre
+               System.out.print(vectorNombre[nombre]+ "  ");//Hace la llamada vector nombre
            }
        }
-        public void mostrarPrecio(){
-           for ( precio = 0; precio<intPrecio.length; precio ++){
-               System.out.print("\n"+intPrecio[precio]+"   ");
+        public void mostrarPrecio(){//Clase para mostrar el vector precio
+           for ( precio = 0; precio<intPrecio.length; precio ++){//Ciclo for para mostrar el precio
+               System.out.print("\n"+intPrecio[precio]+"   ");//Hace la llamada al vector y lo imprime.
            }
        }
        public void mostrarCantidad(){
@@ -53,8 +51,8 @@ public class JavaApplication11 {
            System.out.println("\nEl total es: "+total);
        }
                
-   public static void main(String[] args) {
-        // TODO code application logic here
+   public static void main(String[] args) {//Metodo Pricipal main
+        // Metodos de llamada 
         JavaApplication11 nuevo = new JavaApplication11();
         nuevo.datos();
         nuevo.pre();
